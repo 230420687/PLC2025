3fadfbd88011208d921c2808596517b9d8ebcd61
230420687 <230420687@aston.ac.uk>
2025-01-28 12:22:35 +0000

    comments added to haskell code

--- lab2-functionalprimer/haskell/intro1.hs
+++ lab2-functionalprimer/haskell/intro1.hs
@@ -6,7 +6,8 @@ r2 = b^2 + 1/b where b = 2
 
 -- define a function:
 diff a b = abs (a - b)
-
+--the inside differnece function is ran first
+--after the the outside is ran
 r3 = diff (diff a a) a  where a = 1
 r4 = diff (diff a b) b  where a = 1; b = 1
 
@@ -15,11 +16,12 @@ r5 = [1,3,4,1]
 r6 = [1..5]
 
 inc n = n + 1
-
+-- each element in the list is incremented by +1
 r7 = map inc [1..3]
 r8 = map (diff 2) [1..3]
 r9 = map sqrt [1..3]
 
+--this maps each number in the zip list with the sqrt list
 r10 = zip [1..3] (map sqrt [1..3])
 r11 = zip [1..3] (map sqrt [1..2])
 
--- lab2-functionalprimer/haskell/intro2.hs
+++ lab2-functionalprimer/haskell/intro2.hs
@@ -11,16 +11,25 @@ myprogram = print (1 + m)  -- compiler error: m undefined
 m = 1                      -- unless this line is also present
 
 -- sequencing several imperative programs:
+--the do notation allows you to sequence IO actions in an imperative style
 prg1 = do
   print "hello " 
   print name  -- level of indentation is important
 
 -- the same, but using algebra of imperative programs:
+--sequence_ takes a list of monadic action and exectues them sequentially
+--this is an IO action
+
+--a side effect is when something happens that not related to the core functionality of a method or function
+-- e.g. any external effect a function has besides its return value
 prg1' = sequence_ [print "hello ", print name]
 
 -- one imperative program passing value to another:
 prg2 = do
+  --this takes the the string result produced by getLine and assigns it to line
+  --getLine is an IO action that reads a line of input from the cconsole
   line <- getLine
+  --the following line concatenates the youtyped part with the variable line
   putStrLn ("you typed: " ++ line)
 
 main =
