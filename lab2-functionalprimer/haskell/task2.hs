694405fcf2616d241dd6751a5750e0d1b0d090e2
230420687 <230420687@aston.ac.uk>
2025-01-28 13:10:16 +0000

    task 2 compelte

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,9 @@
+onePlusone = do
+    print "1+1=2"
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
+    --just write the function name to call the function
