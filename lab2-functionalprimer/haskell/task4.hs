3f010f0a05b8b3c712bea964c002196420363b5c
230420687 <230420687@aston.ac.uk>
2025-01-29 10:00:38 +0000

    update

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,19 @@
 ask :: String -> IO ()
+--ask is the function name and prompt is the arguement
 ask prompt =
   do
+    --this prints the promt
   putStrLn prompt
   line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+  while line/= "quit"
+    if line == ""
+      then ask prompt
+      else putStrLn ("you said: " ++ reverse line)
 
 main :: IO ()
 main =
   do
+    --assigns the string to the variable prompt
   let prompt = "please say something"
+  --this calls the ask function passing the prompt as the arugement
   ask prompt
